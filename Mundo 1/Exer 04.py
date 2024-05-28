cac81f0818a7896f1b25672d97cab00d1b5503bc
Lucas Paim <lucas.paim19@hotmail.com>
2024-05-27 21:53:02 -0300

    Create Exer 04.py
    Tipo de input

---
+++ Mundo 1/Exer 04.py
@@ -0,0 +1,8 @@
+a = input('Digite alguma coisa: ')
+
+print(type(a))
+print( 'É numérico ? ',a.isnumeric())
+print('É alphanumérico ?',a.isalpha())
+print('É minuscula ?',a.islower())
+print('É titulo ?',a.istitle())
+print('É maiúscula ?',a.isupper())
