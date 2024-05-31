b09367a6c6c5ea22ed01b9918487be5e8d14c46f
Lucas Paim <lucas.paim19@hotmail.com>
2024-05-31 02:21:43 -0300

    Create Exer 06.py

---
+++ Mundo 1/Exer 06.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+
+d = n * 2
+t = n*3
+q = n**(1/2)
+
+print('Seu número é {}.\n o dobro dele é {}.\n o triplo é {}.\n a raiz quadrada é {:.2f}'.format(n,d,t,q))
